399075fe3aec668b515c28c0a2c51c00a29e584d
timrust93 <timrust93@gmail.com>
2024-12-19 23:27:42 +0500

    add dropdowns, validation broken now

--- Forms/Forms/Pages/App/TemplateManagement.cshtml.cs
+++ Forms/Forms/Pages/App/TemplateManagement.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using static Forms.Pages.App.TemplateManagementModel;
 
 namespace Forms.Pages.App
 {
@@ -15,12 +16,27 @@ namespace Forms.Pages.App
             public string Description { get; set; }
         }
 
+        public class QuestionTypeInfo
+        {
+            public int Id { get; set; }
+            public string DisplayName { get; set; }            
+            public int MaxCount { get; set; }
+            public int Left { get; set; }
+        }
+
         [BindProperty]
         public List<TestQuestion> TestQuestionList { get; set; } = new List<TestQuestion>();
+
+        public List<QuestionTypeInfo> QuestionTypeInfos { get; set; } = new List<QuestionTypeInfo>();
         
 
         public void OnGet(int id)
         {
+            QuestionTypeInfos.Add(new QuestionTypeInfo { Id = 0, DisplayName = "Single Line", MaxCount = 4, Left = 1 });
+            QuestionTypeInfos.Add(new QuestionTypeInfo { Id = 1, DisplayName = "Multi Line", MaxCount = 4, Left = 1 });
+            QuestionTypeInfos.Add(new QuestionTypeInfo { Id = 2, DisplayName = "Number", MaxCount = 4, Left = 1 });
+            QuestionTypeInfos.Add(new QuestionTypeInfo { Id = 3, DisplayName = "Checkbox", MaxCount = 4, Left = 1 });
+
             TestQuestionList.Add(new TestQuestion { Title = "Q1", Description = "Q1 Descr" });
             TestQuestionList.Add(new TestQuestion { Title = "Q2", Description = "Q2 Descr" });
             TestQuestionList.Add(new TestQuestion { Title = "Q3", Description = "Q3 Descr" });
